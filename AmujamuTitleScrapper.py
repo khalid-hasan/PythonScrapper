3067964f1da8fcef5a87237aa540e6853c035d6b
Khalid Hasan <38977344+khalid-hasan@users.noreply.github.com>
2019-02-05 15:36:14 +0600

    Initial

---
+++ AmujamuTitleScrapper.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+# infile = open("sitemap.xml","r")
+#
+# contents = infile.read()
+#
+# soup = BeautifulSoup(contents,'xml')
+#
+# for titles in soup.find_all('urlset'):
+#     urls= titles.url.text
+#     print(urls)
+url_list= []
+activity_ids= []
+activity_names= []
+
+start = 1
+end = 24
+
+csv_file = open("all_urls_amujamu.csv", 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['URL', 'Title'])
+
+while start <= end:
+    url=f'https://amujamu.com/search_results?currency=THB&page={start}'
+    headers = {'user-agent': 'my-app/0.0.1'}
+    source= requests.get(url, headers=headers).text
+
+    soup= BeautifulSoup(source, 'lxml')
+
+    for content in soup.find_all('div', class_='media-heading'):
+        try:
+            url = content.a['href']
+            title = content.a['title']
+
+
+            csv_writer.writerow([url, title])
+            print(url)
+            print(title)
+        except Exception as e:
+            pass
+
+    start += 1
